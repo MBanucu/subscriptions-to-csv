9be10fb9dbd56a836262b7d7e45a89a5ed2be9de
MBanucu <michael.banucu@googlemail.com>
2026-01-18 18:47:42 +0100

    chore: bump version to 1.0.1
    - Update version numbers for new release
    - Includes pytest integration in flake checks

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
         {
           subscriptions-to-csv = pkgs.python3Packages.buildPythonPackage {
             pname = "subscriptions-to-csv";
-            version = "1.0.0";
+            version = "1.0.1";
             format = "pyproject";
 
             src = ./.;
--- subscriptions_to_csv/__init__.py
+++ subscriptions_to_csv/__init__.py
@@ -14,7 +14,7 @@ from .converter import (
     ExchangeRateError,
 )
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = [
     "convert_subscriptions",
     "fetch_exchange_rate",
